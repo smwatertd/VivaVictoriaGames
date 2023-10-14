0dcf8fe4bffb0c407aa8a2dd67903db18d159a65
smwatertd <korablev.danil.smtd@mail.ru>
2023-10-14 16:08:42 +0500

    add AppSettings

---
+++ src/core/settings/__init__.py
@@ -0,0 +1,4 @@
+from core.settings.app_settings import AppSettings
+
+
+app = AppSettings()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
-from core import get_production_app
+from core import get_production_app, settings
+
+import uvicorn
 
 
 app = get_production_app()
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host=settings.app.host,
+        port=settings.app.port,
+        reload=True,
+    )
