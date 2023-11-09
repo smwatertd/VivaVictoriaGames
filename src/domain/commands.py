1bad3bcedc1bf881c91375e3bd89d7edca4b52aa
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-09 15:17:03 +0500

    add domain commands

--- src/domain/commands.py
+++ src/domain/commands.py
@@ -5,17 +5,13 @@ class Command(BaseModel):
     pass
 
 
-class ConnectUser(Command):
+class AddUser(Command):
     game_id: int
     user_id: int
     username: str
 
 
-class DisconnectUser(Command):
+class RemoveUser(Command):
     game_id: int
     user_id: int
     username: str
-
-
-class GetQuestion(Command):
-    game_pk: int
