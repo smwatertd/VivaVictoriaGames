6418ec7d12fcb71e3820a9fd6daf1e4f4daa0b7e
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-10 00:06:36 +0500

    update Channel: replace implementation to class

--- src/infrastructure/adapters/channels.py
+++ src/infrastructure/adapters/channels.py
@@ -1,22 +1,30 @@
-from infrastructure.adapters.consumers import RedisConsumer
-from infrastructure.ports import Channel, WebSocketConnection
+import asyncio
+
+from infrastructure.ports.consumers import Consumer
+from infrastructure.ports.websocket_connections import WebSocketConnection
 
 
-class RedisChannel(Channel):
-    def __init__(self, websocket: WebSocketConnection) -> None:
-        super().__init__(websocket)
-        self.consumer = RedisConsumer(self._wait_for_message)
+class Channel:
+    def __init__(
+        self,
+        websocket: WebSocketConnection,
+        message_consumer: Consumer,
+    ) -> None:
+        self._websocket = websocket
+        self._message_consumer = message_consumer
+        self._wait_for_message_task: asyncio.Task | None = None
 
-    async def _wait_for_message(self) -> None:
-        async for message in self.consumer.pubsub.listen():
-            # TODO: Fix this
-            try:
-                await self.websocket.send_json(str(message))
-            except RuntimeError:
-                pass
+    async def wait_for_message(self) -> None:
+        self._wait_for_message_task = asyncio.ensure_future(self._wait_for_message())
 
     async def subscribe(self, group: str) -> None:
-        await self.consumer.subscribe(group)
+        await self._message_consumer.subscribe(group)
 
     async def unsubscribe(self, group: str) -> None:
-        await self.consumer.unsubscribe(group)
+        assert self._wait_for_message_task, 'wait_for_message must be called first'
+        self._wait_for_message_task.cancel()
+        await self._message_consumer.unsubscribe(group)
+
+    async def _wait_for_message(self) -> None:
+        async for message in self._message_consumer.listen():
+            await self._websocket.send_bytes(message['data'])
--- src/infrastructure/ports/__init__.py
+++ src/infrastructure/ports/__init__.py
@@ -1,5 +1,3 @@
-from infrastructure.ports.channel_layers import ChannelLayer
-from infrastructure.ports.channels import Channel
 from infrastructure.ports.consumers import Consumer
 from infrastructure.ports.producers import Producer
 from infrastructure.ports.unit_of_work import UnitOfWork
