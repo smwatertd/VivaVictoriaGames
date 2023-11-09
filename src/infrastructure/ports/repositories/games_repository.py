bbe3d576b1624a006e0b7c8d0702d3cdad48d622
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-10 00:28:52 +0500

    add Repository abstract class

--- src/infrastructure/ports/repositories/games_repository.py
+++ src/infrastructure/ports/repositories/games_repository.py
@@ -2,10 +2,13 @@ from abc import ABC, abstractmethod
 
 from domain.models import Game
 
+from infrastructure.ports.repositories.repository import Repository
 
-class GamesRepository(ABC):
+
+class GamesRepository(Repository, ABC):
     def __init__(self) -> None:
-        self.seen: set[Game] = set()
+        super().__init__()
+        self.seen: set[Game]
 
     @abstractmethod
     async def get(self, pk: int) -> Game:
