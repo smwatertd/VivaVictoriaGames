4ee14cc669ff20fae8ddaf2d08cb8d5e0e61de83
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-09 15:16:24 +0500

    add domain exceptions

--- src/domain/exceptions.py
+++ src/domain/exceptions.py
@@ -1,10 +1,35 @@
-class GameIsFull(Exception):
+class AddPlayerError(Exception):
     pass
 
 
+class PlayerAlreadyAdded(AddPlayerError):
+    pass
+
+
+class GameIsFull(AddPlayerError):
+    pass
+
+
+class AttackError(Exception):
+    pass
+
+
+class NotYourTurn(AttackError):
+    pass
+
+
+class FieldNotFound(AttackError):
+    pass
+
+
+class AlreadyOwned(AttackError):
+    pass
+
+
+class PlayerNotFound(AttackError):
+    pass
+
+
+# TODO: Add more details
 class GameInvalidState(Exception):
-    pass
-
-
-class InvalidMessage(Exception):
     pass
