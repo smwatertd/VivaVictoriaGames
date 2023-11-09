6679ac1fbf58ea8de71d0f4b3b4035ac07951e69
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-10 00:30:14 +0500

    add FieldsRepository

--- src/infrastructure/ports/repositories/__init__.py
+++ src/infrastructure/ports/repositories/__init__.py
@@ -1,1 +1,3 @@
+from infrastructure.ports.repositories.fields_repository import FieldsRepository
 from infrastructure.ports.repositories.games_repository import GamesRepository
+from infrastructure.ports.repositories.players_repository import PlayersRepository
