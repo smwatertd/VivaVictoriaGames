25d818452e0236d6e50ef707c2e2de526ad9cbc3
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-10 00:11:13 +0500

    add RedisProducer

--- src/infrastructure/adapters/producers.py
+++ src/infrastructure/adapters/producers.py
@@ -1,3 +1,5 @@
+import json
+
 import aioredis
 
 from infrastructure.ports import Producer
@@ -5,7 +7,7 @@ from infrastructure.ports import Producer
 
 class RedisProducer(Producer):
     def __init__(self) -> None:
-        self.redis = aioredis.Redis(host='localhost', port=6379, db=0)
+        self.redis = aioredis.Redis(host='localhost', port=6379, db=0, encoding='utf-8')
 
-    async def publish(self, channel: str, message: dict) -> None:
-        await self.redis.publish(channel, str(message))
+    async def publish(self, group: str, data: dict[str, str]) -> None:
+        await self.redis.publish(group, json.dumps(data))
--- src/infrastructure/ports/producers.py
+++ src/infrastructure/ports/producers.py
@@ -1,5 +1,7 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Producer(ABC):
-    pass
+    @abstractmethod
+    async def publish(self, group: str, data: dict[str, str]) -> None:
+        pass
