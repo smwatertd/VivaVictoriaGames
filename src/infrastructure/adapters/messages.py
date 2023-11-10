653a859dd5fe6ef8904632e5715e8cdabfc283de
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-11 01:52:19 +0500

    add RabbitMQMessage

--- src/infrastructure/adapters/messages.py
+++ src/infrastructure/adapters/messages.py
@@ -2,6 +2,8 @@ import json
 
 from infrastructure.ports import Message, MessageType
 
+from pika.spec import Basic, BasicProperties
+
 
 class RedisMessage(Message):
     def __init__(self, body: dict[str, str | bytes]) -> None:
@@ -15,6 +17,26 @@ class RedisMessage(Message):
 
     def get_payload_type(self) -> str:
         return self._payload.get('event_type', 'unknown')
+
+    def get_payload(self) -> dict:
+        return self._payload
+
+
+class RabbitMQMessage(Message):
+    def __init__(
+        self,
+        deliver: Basic.Deliver,
+        properties: BasicProperties,
+        payload: bytes,
+    ) -> None:
+        self._headers = properties.headers
+        self._payload = json.loads(payload)
+
+    def get_message_type(self) -> MessageType:
+        return MessageType(self._headers.get('message_type', 'unknown'))
+
+    def get_payload_type(self) -> str:
+        return self._headers.get('payload_type', 'unknown')
 
     def get_payload(self) -> dict:
         return self._payload
