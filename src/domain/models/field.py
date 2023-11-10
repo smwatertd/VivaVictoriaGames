411e10633f717ffcc0ff7a71534961d9db8fe61b
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-10 18:51:26 +0500

    update Field: add dunder eq

--- src/domain/models/field.py
+++ src/domain/models/field.py
@@ -1,3 +1,4 @@
+from domain import exceptions
 from domain.models.model import Model
 from domain.models.player import Player
 
@@ -11,11 +12,24 @@ class Field(Model):
         super().__init__(pk)
         self._owner = owner
 
+    def __repr__(self) -> str:
+        return f'Field(pk={self._pk}, owner={self._owner})'
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, Field):
+            return False
+        return self._pk == __value._pk
+
     def is_captured(self) -> bool:
         return self._owner is not None
 
-    def can_be_attacked(self, attacker: Player) -> bool:
-        return self._owner is None or self._owner == attacker
+    def ensure_can_capture(self, capturer: Player) -> None:
+        if self._owner == capturer:
+            raise exceptions.AlreadyOwned
 
-    def __repr__(self) -> str:
-        return f'Field(pk={self._pk}, owner={self._owner})'
+    def get_owner(self) -> Player:
+        assert self._owner
+        return self._owner
+
+    def set_owner(self, new_owner: Player) -> None:
+        self._owner = new_owner
