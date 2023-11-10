0c9d51d6dc9b277caa35e409a99d1514b327b577
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-11 01:40:32 +0500

    add MessageDispatcher

--- src/infrastructure/adapters/__init__.py
+++ src/infrastructure/adapters/__init__.py
@@ -1,5 +1,5 @@
-# from infrastructure.adapters.channel_layers import RedisChannelLayer
-# from infrastructure.adapters.channels import RedisChannel
 from infrastructure.adapters.channel_layers import ChannelLayer
 from infrastructure.adapters.channels import Channel
+from infrastructure.adapters.message_dispatcher import MessageDispatcher
+from infrastructure.adapters.message_parser import MessageParser
 from infrastructure.adapters.unit_of_work import UnitOfWorkAdapter
