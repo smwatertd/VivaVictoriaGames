de14d5bbfcd2c2167054fad45706c96688ff6529
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-11 01:45:20 +0500

    add Message abstract class

--- src/infrastructure/ports/__init__.py
+++ src/infrastructure/ports/__init__.py
@@ -1,4 +1,7 @@
+from infrastructure.ports.channels import AbstractChannel
 from infrastructure.ports.consumers import Consumer
+from infrastructure.ports.message_types import MessageType
+from infrastructure.ports.messages import Message
 from infrastructure.ports.producers import Producer
 from infrastructure.ports.unit_of_work import UnitOfWork
 from infrastructure.ports.websocket_connections import WebSocketConnection
