7eabbcb6511f7c8c6465ae395bde129b5d42f6c4
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-24 01:19:57 +0500

    add FieldEntity

--- src/infrastructure/adapters/entities.py
+++ src/infrastructure/adapters/entities.py
@@ -16,7 +16,7 @@ class GameEntity(Base):
 
     question: Mapped['QuestionEntity'] = relationship('QuestionEntity', uselist=False, back_populates='game')
     players: Mapped[list['PlayerEntity']] = relationship('PlayerEntity', back_populates='game')
-    # fields: Mapped[list['FieldEntity']] = relationship('FieldEntity', back_populates='game')
+    fields: Mapped[list['FieldEntity']] = relationship('FieldEntity', back_populates='game')
 
     def __repr__(self) -> str:
         return f'Game(pk={self.pk}, state={self.state}, round_number={self.round_number})'
@@ -30,24 +30,24 @@ class PlayerEntity(Base):
     game_id: Mapped[int] = mapped_column(ForeignKey('games.pk'))
 
     game: Mapped['GameEntity'] = relationship('GameEntity', uselist=False, back_populates='players')
-    # fields: Mapped[list['FieldEntity']] = relationship('FieldEntity', back_populates='owner')
+    fields: Mapped[list['FieldEntity']] = relationship('FieldEntity', back_populates='owner')
 
     def __repr__(self) -> str:
         return f'Player(pk={self.pk}, username={self.username})'
 
 
-# class FieldEntity(Base):
-#     __tablename__ = 'fields'
+class FieldEntity(Base):
+    __tablename__ = 'fields'
 
-#     pk: Mapped[int] = mapped_column(primary_key=True)
-#     game_id: Mapped[int] = mapped_column(ForeignKey('games.pk'))
-#     owner_id: Mapped[int | None] = mapped_column(ForeignKey('players.pk'))
+    pk: Mapped[int] = mapped_column(primary_key=True)
+    game_id: Mapped[int] = mapped_column(ForeignKey('games.pk'))
+    owner_id: Mapped[int | None] = mapped_column(ForeignKey('players.pk'))
 
-#     owner: Mapped['PlayerEntity'] = relationship('PlayerEntity', uselist=False, back_populates='fields')
-#     game: Mapped['GameEntity'] = relationship('GameEntity', uselist=False, back_populates='fields')
+    owner: Mapped['PlayerEntity'] = relationship('PlayerEntity', uselist=False, back_populates='fields')
+    game: Mapped['GameEntity'] = relationship('GameEntity', uselist=False, back_populates='fields')
 
-#     def __repr__(self) -> str:
-#         return f'Field(pk={self.pk}, owner={self.owner})'
+    def __repr__(self) -> str:
+        return f'Field(pk={self.pk}, owner={self.owner})'
 
 
 class QuestionEntity(Base):
