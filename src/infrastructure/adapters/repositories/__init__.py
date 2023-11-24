76ec76a496d2074af4b8d00031bcff4c0e7014cc
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-25 01:07:24 +0500

    delete InMemoryFieldsRepository

--- src/infrastructure/adapters/repositories/__init__.py
+++ src/infrastructure/adapters/repositories/__init__.py
@@ -1,6 +1,3 @@
-from infrastructure.adapters.repositories.fields_repository import (
-    InMemoryFieldsRepository,
-    SQLAlchemyFieldsRepository,
-)
+from infrastructure.adapters.repositories.fields_repository import SQLAlchemyFieldsRepository
 from infrastructure.adapters.repositories.games_repository import SQLAlchemyGamesRepository
 from infrastructure.adapters.repositories.players_repository import SQLAlchemyPlayersRepository
--- src/infrastructure/adapters/repositories/fields_repository.py
+++ src/infrastructure/adapters/repositories/fields_repository.py
@@ -1,15 +1,5 @@
-from domain.models import Field
-
-from infrastructure.adapters.repositories.games_repository import InMemoryGamesRepository
 from infrastructure.ports.repositories import FieldsRepository
 
 
-class InMemoryFieldsRepository(FieldsRepository):
-    fields: dict[int, Field] = {}
-
-    async def get(self, pk: int) -> Field:
-        for _, game in InMemoryGamesRepository.games.items():
-            for field in game._fields:
-                if field._pk == pk:
-                    return field
-        return Field(pk=pk)
+class SQLAlchemyFieldsRepository(FieldsRepository):
+    pass
