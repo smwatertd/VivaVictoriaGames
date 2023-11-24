4171407561e235c7488497457d4ca334306e29dd
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-25 01:06:37 +0500

    delete InMemoryGamesRepository

--- src/infrastructure/adapters/repositories/__init__.py
+++ src/infrastructure/adapters/repositories/__init__.py
@@ -2,8 +2,5 @@ from infrastructure.adapters.repositories.fields_repository import (
     InMemoryFieldsRepository,
     SQLAlchemyFieldsRepository,
 )
-from infrastructure.adapters.repositories.games_repository import (
-    InMemoryGamesRepository,
-    SQLAlchemyGamesRepository,
-)
+from infrastructure.adapters.repositories.games_repository import SQLAlchemyGamesRepository
 from infrastructure.adapters.repositories.players_repository import SQLAlchemyPlayersRepository
--- src/infrastructure/adapters/repositories/games_repository.py
+++ src/infrastructure/adapters/repositories/games_repository.py
@@ -1,15 +1,5 @@
-from domain.enums import GameState
-from domain.models import Field, Game
-
 from infrastructure.ports.repositories import GamesRepository
 
 
-class InMemoryGamesRepository(GamesRepository):
-    games = {
-        1: Game(pk=1, players=[], state=GameState.PLAYERS_WAITING, fields=[Field(pk=i) for i in range(1, 10)]),
-        # 2: Game(pk=2, players=[], state=GameState.PLAYERS_WAITING, fields=[Field(pk=i) for i in range(10, 20)]),
-    }
-
-    async def get(self, pk: int) -> Game:
-        self.seen.add(self.games[pk])
-        return self.games[pk]
+class SQLAlchemyGamesRepository(GamesRepository):
+    pass
