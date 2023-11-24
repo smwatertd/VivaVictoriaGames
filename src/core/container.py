e1cfde2d3ba9f2018e9be6c17ed7272800d17500
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-25 01:18:45 +0500

    update Container: remove repositories init

--- src/core/container.py
+++ src/core/container.py
@@ -2,17 +2,10 @@ from typing import Any
 
 from dependency_injector import containers, providers
 
-from infrastructure.adapters import (
-    ChannelLayer,
-    MessageDispatcher,
-    MessageParser,
-    UnitOfWorkAdapter,
-)
-from infrastructure.adapters import repositories as adapters_repositories
+from infrastructure import adapters
 from infrastructure.adapters.consumers import RabbitMQConsumer
-from infrastructure.adapters.producers import RabbitMQProducer, RedisProducer
+from infrastructure.adapters.producers import RabbitMQProducer
 from infrastructure.ports import UnitOfWork
-from infrastructure.ports import repositories as ports_repositories
 
 from services.handlers import COMMAND_HANDLERS, EVENT_HANDLERS
 from services.messagebus import MessageBus
@@ -20,18 +13,6 @@ from services.messagebus import MessageBus
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
-
-    games_repository: ports_repositories.GamesRepository = providers.Factory(
-        adapters_repositories.InMemoryGamesRepository,
-    )
-
-    players_repository: ports_repositories.PlayersRepository = providers.Factory(
-        adapters_repositories.InMemoryPlayersRepository,
-    )
-
-    fields_repository: ports_repositories.FieldsRepository = providers.Factory(
-        adapters_repositories.InMemoryFieldsRepository,
-    )
 
     event_producer: Any = providers.Factory(
         RabbitMQProducer,
@@ -48,31 +29,23 @@ class Container(containers.DeclarativeContainer):
     )
 
     channel_layer: Any = providers.Singleton(
-        ChannelLayer,
+        adapters.ChannelLayer,
     )
 
     message_parser: Any = providers.Singleton(
-        MessageParser,
+        adapters.MessageParser,
     )
 
     message_dispatcher: Any = providers.Factory(
-        MessageDispatcher,
+        adapters.MessageDispatcher,
         consumer=event_consumer,
         parser=message_parser,
         messagebus=messagebus,
     )
 
-    message_producer: Any = providers.Factory(
-        RedisProducer,
-    )
-
     unit_of_work: UnitOfWork = providers.Factory(
-        UnitOfWorkAdapter,
-        games=games_repository,
-        players=players_repository,
-        fields=fields_repository,
+        adapters.SQLAlchemyUnitOfWork,
         event_producer=event_producer,
-        message_producer=message_producer,
     )
 
 
