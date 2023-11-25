33bceec424bc954d1dcca5002e68aebe35911be6
smwatertd <korablev.danil.smtd@mail.ru>
2023-11-25 22:48:44 +0500

    add models package Question import

--- src/domain/models/__init__.py
+++ src/domain/models/__init__.py
@@ -2,3 +2,4 @@ from domain.models.answer import Answer
 from domain.models.field import Field
 from domain.models.game import Game
 from domain.models.player import Player
+from domain.models.question import Question
