52304922596fd2683fedd1bc1ac762b6b543b788
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-02 06:03:09 +0500

    update SQLAlchemyGamesRepository: add get

--- src/infrastructure/adapters/repositories/games_repository.py
+++ src/infrastructure/adapters/repositories/games_repository.py
@@ -1,5 +1,30 @@
+from domain.models import Game
+
 from infrastructure.ports.repositories import GamesRepository
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 
 class SQLAlchemyGamesRepository(GamesRepository):
-    pass
+    def __init__(self, session: AsyncSession) -> None:
+        super().__init__()
+        self._session = session
+
+    async def get(self, id: int) -> Game:
+        result = await self._session.execute(
+            select(Game)
+            .where(Game.id == id)
+            .options(
+                joinedload(Game._players),
+                joinedload(Game._fields),
+                joinedload(Game._question),
+            ),
+        )
+        game = result.scalars().first()
+        if game is None:
+            # TODO: replace with proper exception
+            raise ValueError(f'Game {id} not found')
+        self.seen.add(game)
+        return game
