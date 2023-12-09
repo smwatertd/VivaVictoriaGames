b87122b04948df1760d24d4c562ab4faac250341
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-09 22:55:54 +0500

    delete adapters messages

--- src/infrastructure/adapters/messages.py
+++ src/infrastructure/adapters/messages.py
@@ -1,42 +1,10 @@
-import json
+from dataclasses import dataclass
 
-from infrastructure.ports import Message, MessageType
-
-from pika.spec import Basic, BasicProperties
+from infrastructure.adapters.message_types import MessageType
 
 
-class RedisMessage(Message):
-    def __init__(self, body: dict[str, str | bytes]) -> None:
-        self._payload = json.loads(body.get('data', ''))
-
-    def __repr__(self) -> str:
-        return f'RedisMessage(payload={self._payload})'
-
-    def get_message_type(self) -> MessageType:
-        return MessageType.EVENT
-
-    def get_payload_type(self) -> str:
-        return self._payload.get('event_type', 'unknown')
-
-    def get_payload(self) -> dict:
-        return self._payload
-
-
-class RabbitMQMessage(Message):
-    def __init__(
-        self,
-        deliver: Basic.Deliver,
-        properties: BasicProperties,
-        payload: bytes,
-    ) -> None:
-        self._headers = properties.headers
-        self._payload = json.loads(payload)
-
-    def get_message_type(self) -> MessageType:
-        return MessageType(self._headers.get('message_type', 'unknown'))
-
-    def get_payload_type(self) -> str:
-        return self._headers.get('payload_type', 'unknown')
-
-    def get_payload(self) -> dict:
-        return self._payload
+@dataclass
+class Message:
+    type: MessageType
+    payload_type: str
+    payload: dict
