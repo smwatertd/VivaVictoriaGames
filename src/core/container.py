1b25fdc63402c95ca22236987fc87bda7865da24
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-09 22:54:47 +0500

    update Container: add mypy ignore type

--- src/core/container.py
+++ src/core/container.py
@@ -1,11 +1,10 @@
-from typing import Any
+from typing import Type
+
+from core.settings import rabbitmq_settings, redis_settings
 
 from dependency_injector import containers, providers
 
-from infrastructure import adapters
-from infrastructure.adapters.consumers import RabbitMQConsumer
-from infrastructure.adapters.producers import RabbitMQProducer
-from infrastructure.ports import UnitOfWork
+from infrastructure import adapters, ports
 
 from services.handlers import COMMAND_HANDLERS, EVENT_HANDLERS
 from services.messagebus import MessageBus
@@ -14,39 +13,68 @@ from services.messagebus import MessageBus
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
 
-    event_producer: Any = providers.Factory(
-        RabbitMQProducer,
-    )
+    event_factory: Type[adapters.EventFactory] = providers.Factory(
+        adapters.EventFactory,
+    )  # type: ignore
 
-    event_consumer: Any = providers.Factory(
-        RabbitMQConsumer,
-    )
+    command_factory: Type[adapters.CommandFactory] = providers.Factory(
+        adapters.CommandFactory,
+    )  # type: ignore
 
-    messagebus: Any = providers.Singleton(
+    message_serializer: Type[adapters.MessageSerializer] = providers.Factory(
+        adapters.MessageSerializer,
+        event_factory=event_factory,
+        command_factory=command_factory,
+    )  # type: ignore
+
+    message_producer: Type[ports.Producer] = providers.Factory(
+        adapters.RabbitMQProducer,
+        host=rabbitmq_settings.host,
+        port=rabbitmq_settings.port,
+        virtual_host=rabbitmq_settings.virtual_host,
+        exchange=rabbitmq_settings.exchange,
+    )  # type: ignore
+
+    message_consumer: Type[ports.MessageConsumer] = providers.Factory(
+        adapters.RabbitMQMessageConsumer,
+        host=rabbitmq_settings.host,
+        port=rabbitmq_settings.port,
+        virtual_host=rabbitmq_settings.virtual_host,
+    )  # type: ignore
+
+    chat_message_producer: Type[ports.Producer] = providers.Factory(
+        adapters.RedisProducer,
+        host=redis_settings.host,
+        port=redis_settings.port,
+        db=redis_settings.db,
+        encoding=redis_settings.default_encoding,
+    )  # type: ignore
+
+    chat_message_consumer: Type[ports.ChatMessageConsumer] = providers.Factory(
+        adapters.RedisChatMessageConsumer,
+        host=redis_settings.host,
+        port=redis_settings.port,
+        db=redis_settings.db,
+        encoding=redis_settings.default_encoding,
+        ignore_subscribe_messages=True,
+    )  # type: ignore
+
+    messagebus: Type[MessageBus] = providers.Singleton(
         MessageBus,
         command_handlers=COMMAND_HANDLERS,
         event_handlers=EVENT_HANDLERS,
-    )
+    )  # type: ignore
 
-    channel_layer: Any = providers.Singleton(
-        adapters.ChannelLayer,
-    )
-
-    message_parser: Any = providers.Singleton(
-        adapters.MessageParser,
-    )
-
-    message_dispatcher: Any = providers.Factory(
-        adapters.MessageDispatcher,
-        consumer=event_consumer,
-        parser=message_parser,
-        messagebus=messagebus,
-    )
-
-    unit_of_work: UnitOfWork = providers.Factory(
+    unit_of_work: Type[ports.UnitOfWork] = providers.Factory(
         adapters.SQLAlchemyUnitOfWork,
-        event_producer=event_producer,
-    )
+        event_producer=message_producer,
+        chat_message_producer=chat_message_producer,
+        serializer=message_serializer,
+    )  # type: ignore
+
+    channel_layer: Type[adapters.ChannelLayer] = providers.Factory(
+        adapters.ChannelLayer,
+    )  # type: ignore
 
 
 container = Container()
