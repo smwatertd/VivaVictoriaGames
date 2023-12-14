573473b86ffcbbc73de9c57f30c0481f612f9a79
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-14 17:59:13 +0500

    remove Model inheratence from models

--- src/domain/models/field.py
+++ src/domain/models/field.py
@@ -1,31 +1,16 @@
-from domain import exceptions
-from domain.models.model import Model
 from domain.models.player import Player
 
 
-class Field(Model):
-    def __init__(
-        self,
-        id: int,
-        owner: Player | None = None,
-    ) -> None:
-        super().__init__(id)
+class Field:
+    def __init__(self, id: int, owner: Player | None = None) -> None:
+        self.id = id
         self._owner = owner
 
     def __repr__(self) -> str:
         return f'Field(id={self.id}, owner={self._owner})'
 
-    def __eq__(self, __value: object) -> bool:
-        if not isinstance(__value, Field):
-            return False
-        return self.id == __value.id
-
     def is_captured(self) -> bool:
         return self._owner is not None
-
-    def ensure_can_capture(self, capturer: Player) -> None:
-        if self._owner == capturer:
-            raise exceptions.AlreadyOwned
 
     def get_owner(self) -> Player | None:
         return self._owner
--- src/domain/models/player.py
+++ src/domain/models/player.py
@@ -1,13 +1,19 @@
-from domain.models.model import Model
+from domain.models.answer import Answer
 
 
-class Player(Model):
-    def __init__(self, id: int, username: str) -> None:
-        super().__init__(id)
-        self.username = username
+class Player:
+    def __init__(self, id: int, answer: Answer | None) -> None:
+        self.id = id
+        self._answer = answer
 
     def __repr__(self) -> str:
-        return f'Player(id={self.id}, username={self.username})'
+        return f'Player(id={self.id}, answer={self._answer})'
 
     def __hash__(self) -> int:
         return hash(self.id)
+
+    def set_answer(self, answer: Answer) -> None:
+        self._answer = answer
+
+    def is_answered(self) -> bool:
+        return self._answer is not None
