31f48193886d63750127e979e0db43fb664bb426
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-19 19:27:54 +0500

    update Field: replace attr id to _id

--- src/domain/models/field.py
+++ src/domain/models/field.py
@@ -3,11 +3,14 @@ from domain.models.player import Player
 
 class Field:
     def __init__(self, id: int, owner: Player | None = None) -> None:
-        self.id = id
+        self._id = id
         self._owner = owner
 
     def __repr__(self) -> str:
-        return f'Field(id={self.id}, owner={self._owner})'
+        return f'Field(id={self._id}, owner={self._owner})'
+
+    def get_id(self) -> int:
+        return self._id
 
     def is_captured(self) -> bool:
         return self._owner is not None
