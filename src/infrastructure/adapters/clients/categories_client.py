1da2bd73e44090c7170d0fe51dcaad7eab545c0b
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-19 11:56:35 +0500

    add CategoriesClient

---
+++ src/infrastructure/adapters/clients/categories_client.py
@@ -0,0 +1,15 @@
+from random import choice
+
+from domain.models import Category
+
+from infrastructure.ports.clients import HTTPClient
+
+
+class CategoriesClient:
+    def __init__(self, client: HTTPClient) -> None:
+        self._client = client
+
+    async def random(self) -> Category:
+        categories = await self._client.get_all_categories()
+        category = choice(categories)
+        return Category(id=category.id)
