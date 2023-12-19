16cc74b0d0f3b383b8244407b0e7fa18c8efb7ef
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-19 19:28:45 +0500

    update Player: add reset_answer_id method

--- src/domain/models/player.py
+++ src/domain/models/player.py
@@ -1,19 +1,29 @@
-from domain.models.answer import Answer
-
-
 class Player:
-    def __init__(self, id: int, answer: Answer | None) -> None:
-        self.id = id
-        self._answer = answer
+    def __init__(self, id: int, answer_id: int | None) -> None:
+        self._id = id
+        self._answer_id = answer_id
 
     def __repr__(self) -> str:
-        return f'Player(id={self.id}, answer={self._answer})'
+        return f'Player(id={self._id}, answer={self._answer_id})'
 
-    def __hash__(self) -> int:
-        return hash(self.id)
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Player):
+            return NotImplemented
+        return self._id == other._id
 
-    def set_answer(self, answer: Answer) -> None:
-        self._answer = answer
+    def get_id(self) -> int:
+        return self._id
+
+    def set_answer(self, answer_id: int) -> None:
+        self._answer_id = answer_id
 
     def is_answered(self) -> bool:
-        return self._answer is not None
+        return self._answer_id is not None
+
+    def get_answer_id(self) -> int:
+        if self._answer_id is None:
+            raise ValueError('Player has not answered yet')
+        return self._answer_id
+
+    def reset_answer_id(self) -> None:
+        self._answer_id = None
