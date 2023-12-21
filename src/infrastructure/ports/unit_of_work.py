3d54ddf747ac01f781ff2d877d1bea303936957e
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-21 22:06:30 +0500

    update UnitOfWork: add message broker settings for event publishing

--- src/infrastructure/ports/unit_of_work.py
+++ src/infrastructure/ports/unit_of_work.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Any, Generator
 
+from core.settings import rabbitmq_settings
+
 from domain.events import Event
 
 from infrastructure.adapters.clients import CategoriesClient, QuestionsClient
@@ -45,9 +47,11 @@ class UnitOfWork(ABC):
     async def publish_events(self) -> None:
         for event in self._collect_events():
             message = self.serializer.serialize(event)
-            await self._event_producer.publish('games.events.all', message)
+            await self._event_producer.publish(rabbitmq_settings.games_events_queue, message)
 
     def _collect_events(self) -> Generator[Event, None, None]:
+        if not hasattr(self, 'games'):
+            return
         for game in self.games.seen:
             for event in game.collect_events():
                 yield event
