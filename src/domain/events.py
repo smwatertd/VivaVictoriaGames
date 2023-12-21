379a80fdca51846eae3f75ef4da9978ff0193b0e
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-21 14:09:06 +0500

    delete unused events

--- src/domain/events.py
+++ src/domain/events.py
@@ -5,11 +5,6 @@ class Event(BaseModel):
     pass
 
 
-class FieldEvent(Event):
-    game_pk: int
-    field_pk: int
-
-
 class PlayerAdded(Event):
     game_id: int
     player_id: int
@@ -19,31 +14,9 @@ class GameStarted(Event):
     game_id: int
 
 
-class GameIsFull(Event):
-    game_id: int
-
-
-class GameStateChanged(Event):
-    game_id: int
-    state: str
-
-
-class GameClosed(Event):
-    game_id: int
-
-
-class PlayerTurnChanged(Event):
-    game_id: int
-    player_id: int
-
-
 class PlayerRemoved(Event):
     game_id: int
     player_id: int
-
-
-class FieldAttacked(FieldEvent):
-    attacker_pk: int
 
 
 class FieldCaptured(Event):
@@ -69,10 +42,6 @@ class DuelStarted(Event):
 class QuestionSetted(Event):
     game_id: int
     question_id: int
-
-
-class AllPlayersConnected(Event):
-    game_id: int
 
 
 class RoundStarted(Event):
@@ -104,10 +73,6 @@ class DuelRoundFinished(Event):
     game_id: int
     round_number: int
     correct_answer_id: int
-
-
-class GameInProcess(Event):
-    game_id: int
 
 
 class FieldDefended(Event):
