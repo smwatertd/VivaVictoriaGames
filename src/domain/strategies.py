fd404e5166741b70b24da2a2daa96ee910e2e16e
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-21 19:29:29 +0500

    add ConnectionTimeAndIdentityPlayerTurnSelector

--- src/domain/strategies.py
+++ src/domain/strategies.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from domain.models.player import Player
+from domain.models import Player
 
 
 class PlayerTurnSelector(ABC):
@@ -20,3 +20,12 @@ class IdentityPlayerTurnSelector(PlayerTurnSelector):
 
     def get_order(self, players: list[Player]) -> list[Player]:
         return sorted(players, key=lambda player: -player.get_id())
+
+
+class ConnectionTimeAndIdentityPlayerTurnSelector(PlayerTurnSelector):
+    def select(self, round_number: int, players: list[Player]) -> Player:
+        sorted_players_by_connected_at = self.get_order(players)
+        return sorted_players_by_connected_at[round_number % len(sorted_players_by_connected_at)]
+
+    def get_order(self, players: list[Player]) -> list[Player]:
+        return sorted(players, key=lambda player: (player.get_connected_at(), player.get_id()))
