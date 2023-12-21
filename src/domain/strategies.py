7b34c91d9c301806ec22ad2570509e2518298f7b
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-21 20:52:10 +0500

    update PlayerTurnSelector: add select impl

--- src/domain/strategies.py
+++ src/domain/strategies.py
@@ -7,9 +7,9 @@ if TYPE_CHECKING:
 
 
 class PlayerTurnSelector(ABC):
-    @abstractmethod
     def select(self, round_number: int, players: list['Player']) -> 'Player':
-        pass
+        sorted_players = self.get_order(players)
+        return sorted_players[(round_number - 1) % len(sorted_players)]
 
     @abstractmethod
     def get_order(self, players: list['Player']) -> list['Player']:
@@ -17,18 +17,10 @@ class PlayerTurnSelector(ABC):
 
 
 class IdentityPlayerTurnSelector(PlayerTurnSelector):
-    def select(self, round_number: int, players: list['Player']) -> 'Player':
-        sorted_players_by_id = self.get_order(players)
-        return sorted_players_by_id[round_number % len(sorted_players_by_id)]
-
     def get_order(self, players: list['Player']) -> list['Player']:
         return sorted(players, key=lambda player: -player.get_id())
 
 
 class ConnectionTimeAndIdentityPlayerTurnSelector(PlayerTurnSelector):
-    def select(self, round_number: int, players: list['Player']) -> 'Player':
-        sorted_players_by_connected_at = self.get_order(players)
-        return sorted_players_by_connected_at[round_number % len(sorted_players_by_connected_at)]
-
     def get_order(self, players: list['Player']) -> list['Player']:
         return sorted(players, key=lambda player: (player.get_connected_at(), player.get_id()))
