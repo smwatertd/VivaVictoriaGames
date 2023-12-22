7b2f4a2c0013d18e3c62b769a1cb1de24c0ae98c
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-23 01:01:32 +0500

    delete RedisProducer

--- src/infrastructure/adapters/producers.py
+++ src/infrastructure/adapters/producers.py
@@ -2,38 +2,8 @@ import json
 
 import aio_pika
 
-import aioredis
-
 from infrastructure.adapters.messages import Message
 from infrastructure.ports.producers import Producer
-
-
-class RedisProducer(Producer):
-    def __init__(
-        self,
-        host: str,
-        port: int,
-        db: int,
-        encoding: str,
-    ) -> None:
-        self._redis = aioredis.Redis(
-            host=host,
-            port=port,
-            db=db,
-            encoding=encoding,
-        )
-
-    async def publish(self, group: str, message: Message) -> None:
-        await self._redis.publish(
-            group,
-            json.dumps(
-                {
-                    'type': message.type.value,
-                    'payload_type': message.payload_type,
-                    'payload': message.payload,
-                },
-            ),
-        )
 
 
 class RabbitMQProducer(Producer):
@@ -45,8 +15,7 @@ class RabbitMQProducer(Producer):
 
     async def publish(self, group: str, message: Message) -> None:
         connection = await aio_pika.connect(host=self._host, port=self._port, virtualhost=self._virtual_host)
-        channel = connection.channel()
-        async with channel:
+        async with connection.channel() as channel:
             exchange = await channel.get_exchange(self._exchange)
             await exchange.publish(
                 message=aio_pika.Message(
@@ -58,3 +27,4 @@ class RabbitMQProducer(Producer):
                 ),
                 routing_key=group,
             )
+        await connection.close()
