be45dd92485abe52bd4b445171ecebb0b37ce84a
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-23 02:23:47 +0500

    update MessageHandler: add events_group attr

--- src/infrastructure/adapters/message_handlers.py
+++ src/infrastructure/adapters/message_handlers.py
@@ -8,16 +8,18 @@ from services.messagebus import MessageBus
 class MessageHandler:
     def __init__(
         self,
+        events_group: str,
         consumer: Consumer,
         messagebus: MessageBus,
         serializer: MessageSerializer,
     ) -> None:
+        self._events_group = events_group
         self._consumer = consumer
         self._messagebus = messagebus
         self._serializer = serializer
 
-    async def start(self, group: str) -> None:
-        await self._consumer.listen(group, self.handle)
+    async def start(self) -> None:
+        await self._consumer.listen(self._events_group, self.handle)
 
     async def handle(self, message: Message) -> None:
         await self._messagebus.handle(self._serializer.deserialize(message))
