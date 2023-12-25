6c52aff9b5dd7e89576415f60850a085f3b886bc
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-25 22:34:02 +0500

    rename health_view to health_router

--- src/entrypoints/__init__.py
+++ src/entrypoints/__init__.py
@@ -1,12 +1,12 @@
 from entrypoints.games_router import router as games_router
 from entrypoints.games_view import router as games_view
-from entrypoints.health_view import router as health_view
+from entrypoints.health_router import router as health_router
 
 from fastapi import APIRouter
 
 
 all_views: tuple[APIRouter, ...] = (
-    health_view,
+    health_router,
     games_view,
     games_router,
 )
--- src/entrypoints/health_router.py
+++ src/entrypoints/health_router.py
@@ -8,10 +8,6 @@ router = APIRouter(
 )
 
 
-@router.get(
-    '/health',
-    status_code=200,
-    response_model=HealthSchema,
-)
+@router.get('/health', status_code=200, response_model=HealthSchema)
 def check_health() -> HealthSchema:
     return HealthSchema(status='ok')
