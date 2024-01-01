a3ccd631b1e5572f1dc72099d498794c601ac337
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-01 21:34:22 +0500

    update Duel: make dual as a part of battlings stage

--- src/domain/models/duel.py
+++ src/domain/models/duel.py
@@ -1,6 +1,6 @@
 from core.settings import game_settings
 
-from domain.models.field import Field
+from domain.models.captured_field import CapturedField
 from domain.models.player import Player
 
 
@@ -9,100 +9,70 @@ class Duel:
         self,
         id: int,
         round_number: int,
+        category_id: int | None,
+        correct_answer_id: int | None,
         attacker: Player,
         defender: Player,
-        field: Field,
-        category_id: int,
-        question_id: int,
+        field: CapturedField,
     ) -> None:
         self._id = id
         self._round_number = round_number
+        self._category_id = category_id
+        self._correct_answer_id = correct_answer_id
         self._attacker = attacker
         self._defender = defender
-        self._category_id = category_id
-        self._question_id = question_id
         self._field = field
 
-    def __repr__(self) -> str:
-        return (
-            f'Duel(id={self._id}, round_number={self._round_number}, attacker={self._attacker}, '
-            f'defender={self._defender}, category={self._category_id}, question={self._question_id}, '
-            f'field={self._field})'
-        )
-
-    def start(self, attacker: Player, defender: Player, field: Field) -> None:
+    def start(self, attacker: Player, defender: Player, field: CapturedField) -> None:
         self._round_number = 1
         self._attacker = attacker
         self._defender = defender
         self._field = field
-        self._reset_members_answers()
 
     def set_category(self, category_id: int) -> None:
         self._category_id = category_id
 
-    def set_question(self, question_id: int) -> None:
-        self._question_id = question_id
+    def get_round_number(self) -> int:
+        return self._round_number
 
-    def set_player_answer(self, player: Player, answer_id: int) -> None:
-        player.set_answer(answer_id)
+    def start_round(self) -> None:
+        self._correct_answer_id = None
+        self._attacker.clear_answer()
+        self._defender.clear_answer()
+
+    def set_correct_answer(self, answer: int) -> None:
+        self._correct_answer_id = answer
+
+    def set_player_answer(self, player: Player, answer: int) -> None:
+        if player == self._attacker:
+            self._attacker.set_answer(answer)
+        else:
+            self._defender.set_answer(answer)
 
     def are_all_players_answered(self) -> bool:
         return self._attacker.is_answered() and self._defender.is_answered()
 
-    def increase_round_number(self, value: int = 1) -> None:
-        self._round_number += value
-
-    def is_attacker_won(self) -> bool:
-        return self._field.get_owner() == self._attacker
-
-    def get_attacker(self) -> Player:
-        return self._attacker
-
-    def get_field(self) -> Field:
-        return self._field
-
-    def get_round_number(self) -> int:
-        return self._round_number
-
-    def set_correct_answer_id(self, answer_id: int) -> None:
-        self._correct_answer_id = answer_id
-
-    def get_correct_answer_id(self) -> int:
-        return self._correct_answer_id
-
-    def check_round_outcome(self) -> bool:
-        return (
-            self._round_number == game_settings.duel_max_rounds
-            or (
-                self._attacker.get_answer_id() == self._correct_answer_id
-                and self._defender.get_answer_id() != self._correct_answer_id
+    def is_continuing(self) -> bool:
+        return not (
+            (
+                self._attacker.get_answer() == self._correct_answer_id
+                and self._defender.get_answer() != self._correct_answer_id
             )
-            or (
-                self._defender.get_answer_id() == self._correct_answer_id
-                and self._attacker.get_answer_id() != self._correct_answer_id
-            )
+            or self._round_number == game_settings.duel_max_rounds
         )
 
-    def finish_round(self) -> None:
-        pass
+    def stop(self) -> None:
+        if self._attacker.get_answer() == self._correct_answer_id:
+            if self._defender.get_answer() != self._correct_answer_id:
+                self._field.set_owner(self._attacker)
+            else:
+                self._field.on_defend()
 
-    def finish(self) -> None:
-        if (
-            self._attacker.is_answered()
-            and self._attacker.get_answer_id() == self._correct_answer_id
-            and self._defender.get_answer_id() != self._correct_answer_id
-        ):
-            self._field.set_owner(self._attacker)
-        else:
-            self._field.increase_value(1)
-        self._reset_members_answers()
-
-    def start_round(self) -> None:
-        self._reset_members_answers()
-
-    def get_category_id(self) -> int:
-        return self._category_id
-
-    def _reset_members_answers(self) -> None:
-        self._attacker.reset_answer_id()
-        self._defender.reset_answer_id()
+        self._defender.clear_answer()
+        self._attacker.clear_answer()
+        self._defender = None
+        self._attacker = None
+        self._field = None
+        self._category_id = None
+        self._correct_answer_id = None
+        self._round_number = 0
