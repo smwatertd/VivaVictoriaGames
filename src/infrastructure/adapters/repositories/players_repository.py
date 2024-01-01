55e6d58204e994295bf24ddf6e610b455c12f62c
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-02 01:01:44 +0500

    update SQLAlchemyPlayersRepository: add marked_field joinedload

--- src/infrastructure/adapters/repositories/players_repository.py
+++ src/infrastructure/adapters/repositories/players_repository.py
@@ -1,8 +1,10 @@
-from domain.models import Player
+from domain import models
 
 from infrastructure.ports.repositories import PlayersRepository
 
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 
 class SQLAlchemyPlayersRepository(PlayersRepository):
@@ -10,19 +12,27 @@ class SQLAlchemyPlayersRepository(PlayersRepository):
         super().__init__()
         self._session = session
 
-    async def add(self, player: Player) -> None:
+    async def add(self, player: models.Player) -> None:
         self._session.add(player)
 
-    async def get(self, id: int) -> Player:
-        player = await self._session.get(Player, id)
+    async def get(self, id: int) -> models.Player:
+        result = await self._session.execute(
+            select(models.Player)
+            .where(models.Player._id == id)
+            .options(
+                joinedload(models.Player._fields),
+                joinedload(models.Player._marked_field),
+            ),
+        )
+        player = result.unique().scalar_one_or_none()
         if player is None:
             # TODO: replace with proper exception
             raise ValueError(f'Player {id} not found')
         return player
 
-    async def get_or_create(self, id: int) -> Player:
-        player = await self._session.get(Player, id)
+    async def get_or_create(self, id: int) -> models.Player:
+        player = await self._session.get(models.Player, id)
         if player is None:
-            player = Player(id, None)
-            self._session.add(player)
+            player_id = await self._session.execute(insert(models.Player).values(id=id).returning(models.Player._id))
+            player = await self._session.get(models.Player, player_id.scalar_one())
         return player
