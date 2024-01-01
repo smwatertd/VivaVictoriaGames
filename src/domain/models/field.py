5d5abd4aa56675f9bc0b1f4eb944aa7af98b1ff6
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-01 21:36:19 +0500

    update Field: add MarkedField & CapturedField support

--- src/domain/models/field.py
+++ src/domain/models/field.py
@@ -1,33 +1,62 @@
-from domain.models.player import Player
+from typing import TYPE_CHECKING
+
+from core.settings import game_settings
+
+
+if TYPE_CHECKING:
+    from domain.models.captured_field import CapturedField
+    from domain.models.marked_field import MarkField
+    from domain.models.player import Player
 
 
 class Field:
-    def __init__(self, id: int, value: int, owner: Player | None = None) -> None:
+    def __init__(
+        self,
+        id: int,
+        value: int,
+        captured: 'CapturedField | None',
+        marked_field: 'MarkField | None',
+    ) -> None:
         self._id = id
         self._value = value
-        self._owner = owner
+        self._captured = captured
+        self._marked_field = marked_field
 
     def __repr__(self) -> str:
-        return f'Field(id={self._id}, value={self._value}, owner={self._owner})'
+        return f'Field(id={self._id})'
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, Field):
+            return False
+        return self._id == __value._id
+
+    def __hash__(self) -> int:
+        return hash(self._id)
 
     def get_id(self) -> int:
         return self._id
 
-    def is_captured(self) -> bool:
-        return self._owner is not None
-
-    def get_owner(self) -> Player | None:
-        return self._owner
-
-    def set_owner(self, new_owner: Player) -> None:
-        if self._owner is None:
-            self._value = 1
-        else:
-            self._value += 1
-        self._owner = new_owner
-
     def get_value(self) -> int:
         return self._value
 
-    def increase_value(self, value: int) -> None:
-        self._value += value
+    def mark_as_base(self) -> None:
+        self._value = game_settings.base_field_value
+
+    def on_capture(self) -> None:
+        self._value += 1
+
+    def is_captured(self) -> bool:
+        return self._captured is not None
+
+    def get_owner(self) -> 'Player':
+        if self._captured is None:
+            raise ValueError('Cannot get owner without captured')
+        return self._captured.get_owner()
+
+    def on_defend(self) -> None:
+        self._value += 2
+
+    def get_captured_field(self) -> 'CapturedField':
+        if self._captured is None:
+            raise ValueError('Cannot get captured field without captured')
+        return self._captured
