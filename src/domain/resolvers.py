4528192e76c23520f8520bc1a419333178388d4f
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-01 21:29:39 +0500

    add AnsweredByConflictResolver

--- src/domain/resolvers.py
+++ src/domain/resolvers.py
@@ -11,3 +11,16 @@ class ConflictResolver(ABC):
     @abstractmethod
     def get_winner(self, conflict: 'MarkingConflict', correct_answer: int) -> 'Player':
         pass
+
+
+class AnsweredByConflictResolver(ConflictResolver):
+    def get_winner(self, conflict: 'MarkingConflict', correct_answer: int) -> 'Player':
+        correct_answered = [player for player in conflict.players if player.get_answer() == correct_answer]
+
+        if correct_answered:
+            return sorted(correct_answered, key=lambda player: player.get_answered_at(), reverse=True)[0]
+        return sorted(
+            conflict.players,
+            key=lambda player: (player.is_answered(), player.get_answered_at(), player.get_id()),
+            reverse=True,
+        )[0]
