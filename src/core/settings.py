3d142261e9f6e54bc22a484476274db414a6c6b1
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-01 21:46:41 +0500

    update GameSettings: add preparatory stage settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -60,11 +60,10 @@ class DatabaseSettings(BaseSettings):
 
 class GameSettings(BaseSettings):
     players_count_to_start: int = 2
-    fields_count: int = 9
-    max_rounds: int = 2
-    duel_max_rounds: int = 3
-    round_time_seconds: int = 30
-    duel_round_time_seconds: int = 15
+    fields_count: int = 5
+
+    preparatory_stage_round_time_seconds: int = 30
+    base_field_value: int = 10
 
     model_config = SettingsConfigDict(
         env_file='.env',
