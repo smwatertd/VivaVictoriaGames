59c35f9c17036c98b0b283e69999a17bd4f26b7a
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-04 03:13:44 +0500

    update AnsweredByConflictResolver: refactoring get_winner method

--- src/domain/resolvers.py
+++ src/domain/resolvers.py
@@ -5,22 +5,28 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from domain.models.marking_conflict import MarkingConflict
     from domain.models import Player
+    from domain.models import Answer
 
 
 class ConflictResolver(ABC):
     @abstractmethod
-    def get_winner(self, conflict: 'MarkingConflict', correct_answer: int) -> 'Player':
+    def get_winner(self, conflict: 'MarkingConflict', correct_answer: 'Answer') -> 'Player':
         pass
 
 
 class AnsweredByConflictResolver(ConflictResolver):
-    def get_winner(self, conflict: 'MarkingConflict', correct_answer: int) -> 'Player':
-        correct_answered = [player for player in conflict.players if player.get_answer() == correct_answer]
-
+    def get_winner(self, conflict: 'MarkingConflict', correct_answer: 'Answer') -> 'Player':
+        players = list(conflict.players)
+        correct_answered = self._get_correct_answered_players(players, correct_answer)
         if correct_answered:
-            return sorted(correct_answered, key=lambda player: player.get_answered_at(), reverse=True)[0]
-        return sorted(
-            conflict.players,
-            key=lambda player: (player.is_answered(), player.get_answered_at(), player.get_id()),
-            reverse=True,
-        )[0]
+            return self._get_earliest_answered_player(correct_answered)
+        return self._get_earliest_wrong_answered_player(players)
+
+    def _get_correct_answered_players(self, players: list['Player'], correct_answer: 'Answer') -> list['Player']:
+        return [player for player in players if player.get_answer() == correct_answer]
+
+    def _get_earliest_answered_player(self, players: list['Player']) -> 'Player':
+        return min(players, key=lambda player: player.get_answered_at())
+
+    def _get_earliest_wrong_answered_player(self, players: list['Player']) -> 'Player':
+        return min(players, key=lambda player: (player.is_answered(), player.get_answered_at(), player.get_id()))
