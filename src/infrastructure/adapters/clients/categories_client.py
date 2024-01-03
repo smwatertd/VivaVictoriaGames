6f8b20c4ebbb30eafa5e34a09b75fce0f83bb5e1
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-04 03:21:46 +0500

    update CategoriesClient: returning Category in random method

--- src/infrastructure/adapters/clients/categories_client.py
+++ src/infrastructure/adapters/clients/categories_client.py
@@ -1,5 +1,7 @@
 from random import choice
 
+from domain.models import Category
+
 from infrastructure.ports.clients import HTTPClient
 
 
@@ -7,6 +9,7 @@ class CategoriesClient:
     def __init__(self, client: HTTPClient) -> None:
         self._client = client
 
-    async def random(self) -> int:
+    async def random(self) -> Category:
         categories = await self._client.get_all_categories()
-        return choice(categories).id
+        random_category = choice(categories)
+        return Category(id=random_category.id, name=random_category.name)
