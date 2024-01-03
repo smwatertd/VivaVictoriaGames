f06eabae0743aa90945523b5166d26ae207da77e
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-04 03:22:23 +0500

    update QuestionsClient: returning Question in random_by_category method

--- src/infrastructure/adapters/clients/questions_client.py
+++ src/infrastructure/adapters/clients/questions_client.py
@@ -1,3 +1,5 @@
+from domain.models import Answer, Question
+
 from infrastructure.ports.clients import HTTPClient
 
 
@@ -5,9 +7,11 @@ class QuestionsClient:
     def __init__(self, client: HTTPClient) -> None:
         self._client = client
 
-    async def random_by_category(self, category_id: int) -> int:
+    async def random_by_category(self, category_id: int) -> Question:
         response = await self._client.get_random_question_by_category(category_id)
-        return response.id
+        answers = [Answer(id=answer.id, body=answer.body) for answer in response.answers]
+        [correct_answer] = [Answer(id=answer.id, body=answer.body) for answer in response.answers if answer.is_correct]
+        return Question(id=response.id, body=response.body, answers=answers, correct_answer=correct_answer)
 
     async def get_correct_answer(self, question_id: int) -> int:
         response = await self._client.get_question(question_id)
