e4cf4f47584cebf88169a4125dd342ca6ceb8f95
smwatertd <korablev.danil.smtd@mail.ru>
2024-01-04 03:12:39 +0500

    update Battle: add finish_round method

--- src/domain/models/battle.py
+++ src/domain/models/battle.py
@@ -1,4 +1,7 @@
+from domain.models.answer import Answer
+from domain.models.category import Category
 from domain.models.duel import Duel
+from domain.models.duel_result import DuelResult
 from domain.models.field import Field
 from domain.models.player import Player
 
@@ -12,22 +15,28 @@ class Battle:
     def start(self) -> None:
         self._round_number = 1
 
+    def finish_round(self) -> None:
+        self._round_number += 1
+
     def get_round_number(self) -> int:
         return self._round_number
 
-    def start_duel(self, attacker: Player, defender: Player, field: Field) -> None:
-        self._duel.start(attacker, defender, field.get_captured_field())
+    def start_duel(self, attacker: Player, defender: Player, field: Field, category: Category) -> None:
+        self._duel.start(attacker, defender, field.get_captured_field(), category)
 
     def get_duel_round(self) -> int:
         return self._duel.get_round_number()
 
-    def set_duel_category(self, category: int) -> None:
-        self._duel.set_category(category)
-
     def start_duel_round(self) -> None:
         self._duel.start_round()
 
-    def set_duel_correct_answer(self, answer: int) -> None:
+    def finish_duel_round(self) -> None:
+        self._duel.finish_round()
+
+    def get_duel_category(self) -> Category:
+        return self._duel.get_category()
+
+    def set_duel_correct_answer(self, answer: Answer) -> None:
         self._duel.set_correct_answer(answer)
 
     def set_player_answer(self, player: Player, answer: int) -> None:
@@ -39,5 +48,5 @@ class Battle:
     def is_duel_continuing(self) -> bool:
         return self._duel.is_continuing()
 
-    def stop_duel(self) -> None:
-        self._duel.stop()
+    def stop_duel(self) -> DuelResult:
+        return self._duel.stop()
